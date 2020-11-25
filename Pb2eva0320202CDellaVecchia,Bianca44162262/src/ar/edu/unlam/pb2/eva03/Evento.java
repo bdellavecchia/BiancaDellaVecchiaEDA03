6a536bd8609b09a8e1e81255f895fdf02e10884f
bdellavecchia <71387606+bdellavecchia@users.noreply.github.com>
2020-11-25 08:46:44 -0300

    Add files via upload
    primer commit original

---
+++ Pb2eva0320202CDellaVecchia,Bianca44162262/src/ar/edu/unlam/pb2/eva03/Evento.java
@@ -0,0 +1,16 @@
+package ar.edu.unlam.pb2.eva03;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import ar.edu.unlam.pb2.eva03.enumeradores.TipoDeEvento;
+
+public class Evento {
+
+	private TipoDeEvento tipo;
+	private Integer numeroDeInscripcion;
+	private Map<Integer, Deportista> participantes;
+	
+}
